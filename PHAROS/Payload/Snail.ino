b3982b7df45446f6e3412f4a5113d68ebd9c4fe8
dsp-mark <dsp.engineer.mark@gmail.com>
2025-06-14 14:57:29 -0700

    Updated payload radio

--- PHAROS/Payload/Snail.ino
+++ PHAROS/Payload/Snail.ino
@@ -1,30 +1,11 @@
-#include <LiquidCrystal_I2C.h>
+#include <ArduinoJson.h>
 
-// This is the test set up with the LCD screen!
-
-
-// Thank you for your recommendation, Owen!
-#define RFSerial Serial2 
-
-struct SensorData {
-    float tempF;
-    float pressure_hPa;
-    float yaw;
-    float pitch;
-    float roll;
-};
-
-LiquidCrystal_I2C lcd(0x27,20,4);
+#define RFSerial Serial1 
 
 void setup() {
-  RFSerial.begin(57600);
   Serial.begin(9600);
-
-  lcd.init();
-  lcd.backlight();
-  lcd.setCursor(0,0);
-  lcd.print("Snail is waking up!");
-
+  RFSerial.begin(57600);
+  
   Serial.println("Snail is waking up!");
 
   delay(100);
@@ -32,25 +13,25 @@ void setup() {
 
 void loop() {
   if (RFSerial.available()) {
-    SensorData receivedData;  
-    RFSerial.readBytes((char*)&receivedData, sizeof(receivedData));
-    lcd.clear();
-    // String receivedData = RFSerial.readStringUntil('\n');
-    lcd.setCursor(0,0);
-    lcd.print("Temp: "); lcd.print(receivedData.tempF);
+    StaticJsonDocument<200> doc;
 
-    lcd.setCursor(0,1);
-    lcd.print("Pressure: "); lcd.print(receivedData.pressure_hPa);
+    DeserializationError err = deserializeMsgPack(doc, RFSerial);
 
-    lcd.setCursor(0,2);
-    lcd.print("Y: "); lcd.print(receivedData.yaw);
-    lcd.print(", P: "); lcd.print(receivedData.pitch);
-    lcd.setCursor(0,3);
-    lcd.print(", R: "); lcd.print(receivedData.roll);
+    if (!err) {
+      float tempF = doc["t"];
+      float presHPa = doc["p"];
+      float imuR = doc["r"];
+      float imuI = doc["i"];
+      float imuJ = doc["j"];
+      float imuK = doc["k"];
+      float rockblockSignalQuality = doc["s"];
 
-    Serial.print("Running");
-
+      Serial.print("Temperature (F): "); Serial.println(tempF);
+      Serial.print("Pressure (hPa): "); Serial.println(pressHPa);
+      Serial.print("IMU: ["); Serial.print(imuR); Serial.print(imuI); Serial.print(imuJ); Serial.println(imuK);
+      Serial.print("SignalQuality: "); Serial.print(rockblockSignalQuality);
+    }   
   }
   
-  delay(750);
+  delay(250);
 }
